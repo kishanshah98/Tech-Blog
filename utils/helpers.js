902cac62397de240a607dec64956475660166ab3
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-06-29 19:16:00 -0400

    New files created and started working on routes and handlebars

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return `${new Date(date).getMonth()}/${new Date(date).getDate()}`;
+    },
+  };
+  
